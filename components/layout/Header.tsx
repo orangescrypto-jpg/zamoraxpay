ad0b2a23a44426b27465e5774c76e8bc8c49e63a
orangescrypto-jpg <orangescrypto@gmail.com>
2026-09-11 08:34:41 +0100

    Update Header.tsx

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -41,12 +41,12 @@ function BannerSlider() {
   const current = banners[activeIndex]
 
   const SlideContent = (
-    <div className="relative h-32 w-full overflow-hidden rounded-lg bg-secondary sm:h-40 md:h-48">
+    <div className="relative aspect-[3/1] w-full overflow-hidden rounded-lg bg-secondary">
       <img
         key={current.id}
         src={current.imageUrl}
         alt={current.title ?? "Promotional banner"}
-        className="banner-slide-enter h-full w-full object-cover"
+        className="banner-slide-enter h-full w-full object-contain"
       />
       {current.title && (
         <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black/60 to-transparent p-3">
